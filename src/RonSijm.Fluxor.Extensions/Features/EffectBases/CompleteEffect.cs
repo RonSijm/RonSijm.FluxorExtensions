f4e757ff92008512c9fc402127ed061c32b7977d
Ron Sijm <sijm.ron+github@gmail.com>
2025-03-31 10:39:36 +0200

    - Fluxor Extensions

---
+++ src/RonSijm.Fluxor.Extensions/Features/EffectBases/CompleteEffect.cs
@@ -0,0 +1,9 @@
+﻿namespace RonSijm.Syringe;
+
+public abstract class CompleteEffect<TState, TAction> : EffectWithResult<TAction, TState> where TAction : ICompleteAction<TState>
+{
+    protected override Task<TState> HandleAsync(TAction action)
+    {
+        return Task.FromResult(action.Response);
+    }
+}
